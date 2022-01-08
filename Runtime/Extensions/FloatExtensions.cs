c386b5eec99a136417a29da93616e8ccacb1ecd5
Pierre Roy <lrp.roy@gmail.com>
2022-01-08 16:15:33 +0100

    extensions

---
+++ Runtime/Extensions/FloatExtensions.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace Tools.Extensions
+{
+    /// <summary>
+    /// Extension methods for UnityEngine.Float
+    /// </summary>
+    public static class FloatExtensions
+    {
+        /// <summary>
+        /// Return true if float is almost equal to value, return false otherwise. 
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="value"></param>
+        /// <param name="tolerance"></param>
+        /// <returns></returns>
+        public static bool AlmostEqual(this float self, float value, float tolerance)
+        {
+            return Mathf.Abs(self - value) < tolerance;
+        }
+
+        /// <summary>
+        /// Return true if value is between min and max, return false otherwise
+        /// </summary>
+        /// <param name="self">self</param>
+        /// <param name="min">min value</param>
+        /// <param name="max">max value</param>
+        /// <param name="include">include bounds</param>
+        /// <returns></returns>
+        public static bool InRange(this float self, float min, float max, bool include = true)
+        {
+            return include ? (self >= min && self <= max) : (self > min && self < max);
+        }
+
+        /// <summary>
+        /// Remap value to another range.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="from1"></param>
+        /// <param name="to1"></param>
+        /// <param name="from2"></param>
+        /// <param name="to2"></param>
+        /// <returns></returns>
+        public static float Convert(this float value, float minA, float maxA, float minB, float maxB)
+        {
+            float normal = Mathf.InverseLerp(minA, maxA, value);
+            return Mathf.Lerp(minB, maxB, normal);
+        }
+    }
+}
---
+++ Runtime/Extensions/IntExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Tool.Extensions
+{
+    public static class IntExtensions
+    {
+        /// <summary>
+        /// Returns true if value is between min and max, else returns false.
+        /// </summary>
+        /// <param name="self">self</param>
+        /// <param name="min">min value</param>
+        /// <param name="max">max value</param>
+        /// <param name="include">include bounds</param>
+        /// <returns></returns>
+        public static bool InRange(this int self, int min, int max, bool include = false)
+        {
+            return include ? self >= min && self <= max : self > min && self < max;
+        }
+    }
+}
